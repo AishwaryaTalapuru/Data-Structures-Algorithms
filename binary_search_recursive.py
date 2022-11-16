e5265f0d7a869b3d8b9efbc5369b0ece973dc5ff
aishwarya talapuru <aishwaryatalapuru@aishwaryas-MacBook-Pro.local>
2022-11-16 18:53:10 -0500

    Added hanges for Binary search (Recursive) Process

---
+++ binary_search_recursive.py
@@ -0,0 +1,19 @@
+def binary_search(arr, low, high, value):
+    if low <= high:
+        mid = (low+high)//2
+        if mid < low or mid > high:
+            return False
+        if arr[mid] == value:
+            return True
+        if value > arr[mid]:
+            return binary_search(arr, mid+1, high, value)
+        else:
+            return binary_search(arr, low, mid-1, value)
+    return False
+inputs = [[1, 2, 3, 4, 5], [1, 2, 4, 5], [1, 2, 4, 5], [1, 2, 9, 5, 7]]
+to_find = [3, 3, 6, 0]
+for index in range(len(inputs)):
+    high = len(inputs[index])-1
+    print("Is the element ", to_find[index], " in the list ", inputs[index], ": ", binary_search(inputs[index], 0, high, to_find[index]))
+#Time Complexity - O(n) where n is the size of the input arr
+#Space Compleixty - O(1)
