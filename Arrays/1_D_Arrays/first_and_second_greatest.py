da294e56b3f33060a68b6859607ef404a76566b5
aishwarya talapuru <aishwaryatalapuru@aishwaryas-MacBook-Pro.local>
2024-06-25 21:28:24 -0400

    Added changes

--- Arrays/1_D_Arrays/first_and_second_greatest.py
+++ Arrays/1_D_Arrays/first_and_second_greatest.py
@@ -6,7 +6,7 @@ def first_and_second_greatest(arr):
     if first_max_elem == -2e9:
         #This means that there is no max elem
         #array may be empty
-        return [-1 -1]
+        return [-1, -1]
     second_max_elem = -2e9
     for elem in arr:
         if elem != first_max_elem:
@@ -14,4 +14,26 @@ def first_and_second_greatest(arr):
     if second_max_elem == -2e9:
         second_max_elem = -1
     return [first_max_elem, second_max_elem]
-    
+
+def evaluator(arr):
+    arr = list(set(arr))
+    arr.sort(reverse=True)
+    if len(arr)==0:
+        return [-1, -1]
+    if len(arr)==1:
+        return [arr[0], -1]
+    else:
+        return [arr[0], arr[1]]
+
+#Time Complexity - O(n)
+#Space Complexity - O(1)
+
+test_cases = [[1, 2, 3], [3, 2, 1], [3, 3, 3], [3], []]
+for test_case in test_cases:
+    print("Test case: ", test_case)
+    print("Expected Result:  ", evaluator(test_case))
+    print("Actual Result:  ",first_and_second_greatest(test_case) )
+    if first_and_second_greatest(test_case) == evaluator(test_case):
+        print("Passed")
+    else:
+        print("Failed")
